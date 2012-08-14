8ec5008301334dd0aa34c5db0c3073ef8560be13
Doug Ireton <dougireton@gmail.com>
2012-08-14 10:23:09 -0700

    Change git:// links to https:// links in Install.sh
    git:// links don't work through HTTP proxies.

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-git clone git://github.com/dougireton/vimfiles.git ~/.vim
+git clone https://github.com/dougireton/vimfiles.git ~/.vim
 ln -s ~/.vim/vimrc ~/.vimrc
 ln -s ~/.vim/gvimrc ~/.gvimrc
 mkdir ~/.vim/bundle
-git clone git://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
