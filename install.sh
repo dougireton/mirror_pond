0e02a56f61aceeb70efe754b75855065d503ee91
Ireton, Doug <doug.ireton@nordstrom.com>
2013-10-12 16:12:37 -0700

    Install.sh: mkdir backup, swap, undo dirs
    On OS X, create backup, swap, undo dirs under /Users/xgjo/Library/Vim/

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 vim_dir="$HOME/.vim"
 
 if [ ! -d "$vim_dir" ]; then
-  echo Please git clone mirror_pond to $vim_dir and try again.
+  echo Please git clone mirror_pond to $vim_dir and run this script again...
   exit 1
 fi
 
@@ -14,6 +14,16 @@ vim_version=$(vim --version | grep -o 'Vi IMproved \d\.\d' | cut -d ' ' -f 3)
 if (( $( bc <<< "$vim_version < 7.4" ) )); then
   ln -s $vim_dir/vimrc ~/.vimrc
   ln -s $vim_dir/gvimrc ~/.gvimrc
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+  home_library="$HOME/Library"
+
+  cd $home_library
+  echo Creating backup, swap, and undo dirs under $home_library/Vim/
+  mkdir -p Vim/backup
+  mkdir -p Vim/swap
+  mkdir -p Vim/undo
 fi
 
 mkdir $vim_dir/bundle
